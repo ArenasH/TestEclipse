9591d178757088abc8398b18dd31c0c07ceec4e9
lopp <lopp@marar.marar>
2019-01-09 20:42:00 -0600

    class

--- TestEclipse/src/pil/Main.java
+++ TestEclipse/src/pil/Main.java
@@ -1,7 +1,10 @@
 package pil;
 
-public class Main {public static void main(String args[]) {
+public class Main {
+	public static void main(String args[]) {
 	System.out.println("Hello Wordl!");
 }
-
+class Ja{
+	
+}
 }
